29f19e59bc1b5645b73e8e63129e22c9be294ef7
Adrian Puscasu <apu@f5it.ro>
2019-02-27 00:24:48 +0200

    - adds the .Net Core console project

--- WiredBrainCoffee/ClassLibrary1/Class1.cs
+++ WiredBrainCoffee/ClassLibrary1/Class1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace WiredBrainCoffee.Simulators
+namespace ClassLibrary1
 {
     public class Class1
     {
---
+++ WiredBrainCoffee/WiredBrainCoffee.UI.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using WiredBrainCoffee.Simulators;
+
+namespace WiredBrainCoffee.UI.Wpf
+{
+
+    public partial class MainWindow : Window
+    {
+        private CoffeeMachine _coffeeMachine;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _coffeeMachine = new CoffeeMachine();
+            txtCappuccinoCounter.Text = _coffeeMachine.CounterCappuccino.ToString();
+        }
+
+        private void ButtonMakeCappuccinoClick(object sender, RoutedEventArgs e)
+        {
+            _coffeeMachine.MakeCappuccino();
+            txtCappuccinoCounter.Text = _coffeeMachine.CounterCappuccino.ToString();
+        }
+    }
+}
